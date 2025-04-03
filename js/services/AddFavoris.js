c53df5e8f85a6fa783508e5e8c9edf8f4ca13a93
MatthiasCaroux <lolmonyo@gmail.com>
2025-04-03 08:06:46 +0200

    oui oui

--- js/services/AddFavoris.js
+++ js/services/AddFavoris.js
@@ -1,13 +1,15 @@
-
 export default function addFavoris(card) {
     // permet l'ajout de favoris 
     let favoris = JSON.parse(localStorage.getItem("favoris")) || [];
-    if (!favoris.some(fav => fav.id === card.id)) { // permet de verifier si la carte est déjà dans les favoris
+
+    if (!favoris.some(fav => fav.id === card.id)) {
         favoris.push(card);
         localStorage.setItem("favoris", JSON.stringify(favoris));
         alert(`${card.name} a été ajouté aux favoris !`);
+        location.reload(); // recharge la page après ajout
     } else {
         alert(`${card.name} est déjà dans les favoris.`);
     }
+
     console.log(localStorage.getItem("favoris"));
 }
